6daaa1f69db0940c56796f860fc632f2832434ee
prathammittall <prathamv0106@gmail.com>
2025-10-02 20:14:41 +0530

    Divide and conquer completed, SearchInRotatedArray done

--- Java DSA/DivideConquer.java
+++ Java DSA/DivideConquer.java
@@ -1,27 +1,28 @@
 public class DivideConquer {
     public static void mergeSort(int arr[], int si, int ei) {
-        if (si>=ei) {
-         return;   
+        if (si >= ei) {
+            return;
         }
 
-        int mid = si + (ei - si)/2; // More optimized version of (si+ei)/2 
+        int mid = si + (ei - si) / 2; // More optimized version of (si+ei)/2
 
         mergeSort(arr, si, mid); // left part
-        mergeSort(arr, mid+1, ei); // right part
+        mergeSort(arr, mid + 1, ei); // right part
         merge(arr, si, mid, ei);
     }
 
-    public static void merge(int arr[], int si, int mid, int ei ) {
+    public static void merge(int arr[], int si, int mid, int ei) {
         // left(0,3)=4elements right(4,6)=3elements -> 6-0+1
-        int temp[] = new int[ei-si+1];
+        int temp[] = new int[ei - si + 1];
         int i = si; // iterator for left part
-        int j = mid+1; // iterator for right part
+        int j = mid + 1; // iterator for right part
         int k = 0; // iterator for temp arr
 
         while (i <= mid && j <= ei) {
             if (arr[i] < arr[j]) {
                 temp[k] = arr[i];
-                i++; k++;
+                i++;
+                k++;
             } else {
                 temp[k] = arr[j];
                 j++;
@@ -30,36 +31,37 @@ public class DivideConquer {
         }
 
         // left part
-        while (i<=mid) {
+        while (i <= mid) {
             temp[k++] = arr[i++];
         }
 
-        // right part 
+        // right part
 
-        while (j<=ei) {
+        while (j <= ei) {
             temp[k++] = arr[j++];
         }
 
-        // copy temp to original array 
-        for (k=0; k<temp.length; k++, i++) {
+        // copy temp to original array
+        for (k = 0; k < temp.length; k++, i++) {
             arr[i] = temp[k];
         }
     }
 
     public static void quickSort(int arr[], int si, int ei) {
-        if (si>=ei) return;
+        if (si >= ei)
+            return;
 
         int pIdx = partition(arr, si, ei);
-        quickSort(arr, si, pIdx-1); // left
-        quickSort(arr, pIdx+1, ei); // right
+        quickSort(arr, si, pIdx - 1); // left
+        quickSort(arr, pIdx + 1, ei); // right
     }
 
     public static int partition(int arr[], int si, int ei) {
         int pivot = arr[ei];
-        int i = si-1; // to make place for els smaller than pivot
+        int i = si - 1; // to make place for els smaller than pivot
 
-        for (int j=si; j<ei; j++) {
-            if (arr[j]<=pivot) {
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
                 i++;
                 // swap
                 int temp = arr[j];
@@ -72,20 +74,52 @@ public class DivideConquer {
         arr[ei] = arr[i];
         arr[i] = temp;
         return i;
-        
+
     }
 
-    public static void searchInRotatedArray(int arr[], int target) {
-        
+    public static int searchInRotatedArray(int arr[], int tar, int si, int ei) {
+        // base case
+        if (si > ei)
+            return -1;
+
+        int mid = si + (ei - si) / 2;
+
+        if (arr[mid] == tar) {
+            return mid;
+        }
+
+        // mid on L1
+        if (arr[si] <= arr[mid]) {
+            // case a: left
+            if (arr[si] <= tar && tar <= arr[mid]) {
+                return searchInRotatedArray(arr, tar, si, mid - 1);
+            } else {
+                // case b: right
+                return searchInRotatedArray(arr, tar, mid + 1, ei);
+            }
+        }
+        // mid on L2
+        else {
+            // case c: right
+            if (arr[mid] <= tar && tar <= arr[ei]) {
+                return searchInRotatedArray(arr, tar, mid + 1, ei);
+            } else {
+                // case d: left
+                return searchInRotatedArray(arr, tar, si, mid - 1);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int arr[] = {6, 3, 9, 5, 2, 8};
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 5;
         // mergeSort(arr, 0, arr.length-1);
-        quickSort(arr, 0, arr.length-1);
+        // quickSort(arr, 0, arr.length-1);
 
-        for (int i=0; i<arr.length; i++) {
-            System.out.print(arr[i]+" ");
-        }
+        // for (int i=0; i<arr.length; i++) {
+        // System.out.print(arr[i]+" ");
+        // }
+        int tarIdx = searchInRotatedArray(arr, target, 0, arr.length - 1);
+        System.out.println(tarIdx);
     }
 }
